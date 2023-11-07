173e24a2e5a59fa76333805b9d18817e4b1ad55f
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2023-11-07 18:31:09 +0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    
+    cout << "Nhập một số nguyên: ";
+    cin >> number;
+    
+    cout << "Số bạn vừa nhập là: " << number << endl;
+    
+    return 0;
+}
