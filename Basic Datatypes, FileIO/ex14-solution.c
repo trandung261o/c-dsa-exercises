de4d384f2e13ec3fa43ed4c688d798f983c2787c
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2023-12-30 18:12:44 +0700

    Update ex14-solution.c

--- Basic Datatypes, FileIO/ex14-solution.c
+++ Basic Datatypes, FileIO/ex14-solution.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 enum {SUCCESS, FAIL, MAX_LEN = 81};
 int count = 0;
-void LineReadWrite(FILE *fin, FILE *fout) {
+void LineReadWrite(FILE *fin) {
 	char buff[MAX_LEN];
 	while (fgets(buff, MAX_LEN, fin) != NULL) {
 		++count;
-		fputs(buff, fout);
 		printf("%s", buff);
 	}
 }
 
 int main() {
-	FILE *fptr1, *fptr2;
-	char filename1[] = "lab1w.txt";
+	FILE *fptr2;
 	char filename2[] = "lab1.txt";
 	int reval = SUCCESS;
-	if ((fptr1 = fopen(filename1, "w")) == NULL) {
-		printf("cannot open %s.\n", filename1);
-		reval = FAIL;
-	} else if ((fptr2 = fopen(filename2, "r")) == NULL) {
+	if ((fptr2 = fopen(filename2, "r")) == NULL) {
 		printf("cannot open %s.\n", filename2);
 		reval = FAIL;
 	} else {
 		printf("Reading file %s...done!\n", filename2);
-		LineReadWrite(fptr2, fptr1);
-		printf("This file has %d lines\n", count);
+		LineReadWrite(fptr2);
+		printf("\nThis file has %d lines\n", count);
 		fclose(fptr2);
-		fclose(fptr1);
 	}
 	return reval;
 }
