a779f4a2f47dfd99266d0b606210b6b3c1ab9d7c
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-01-08 04:49:14 +0700

    Update ex9-solution.c

--- Recursion/ex9-solution.c
+++ Recursion/ex9-solution.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <cstring>
-int x[100]; 
-int n = 4;
-int row, col;
+int x[100]; // mảng chứa lời giải
+int n = 4; // kích thước bàn cờ (số hàng)
+int row, col; // tọa độ hàng cột quân hẫu có sẵn
+int count = 0; // số lời giải
 
 void printSolution() {
-	for (int i = 1; i <= n; i++) {
-		printf("%d ", x[i]);
-	}
-	printf("\n");
+	// in lời giải nếu lời giải thuộc trường hợp của quân hẫu xếp sẵn
+	if (x[col] == row) { 
+		count++;
+		for (int i = 1; i <= n; i++) {
+			printf("%d ", x[i]);
+		}
+		printf("\n");
+	} 
 }
 
 int check(int v, int k) {
 	for (int i = 1; i < k; i++) {
-		//kiểm tra v, k có phải hàng cột của con hẫu có sẵn không
-		if (v == row || k == col) return 0;
-
 		if (x[i] == v) return 0;
 		if (x[i] + i == v + k) return 0;
 		if (x[i] - i == v - k) return 0;
@@ -34,16 +36,13 @@ void TRY(int k) {
 }
 
 int main() {
-	memset(x, -1, sizeof(x));
 	//nhập giá trị hàng cột cho con hậu đã có sẵn
 	scanf("%d%d", &row, &col);
-	x[col] = row;
-
 	TRY(1);
-	for (int i = 1; i <= n; i++) {
-		if (x[i] == -1) {
-			printf("-1");
-			return 0;
-		}
+
+	// nếu không có lời giải, in -1
+	if (count == 0) {
+		printf("-1");
+		return 0;
 	}
 }
