590775032d8d5b711f5e6c1caf512e39881efd00
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-01-07 14:26:42 +0700

    Create ex1-solution.c

---
+++ Recursion/ex1-solution.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int tong(int n) {
+	if (n == 1) return 1;
+	else {
+		int s = tong(n-1);
+		return n + tong(n-1);
+	}
+}
+
+int tohop(int k, int n) {
+	if (k == 0 || k == n) return 1;
+	else {
+		int C1 = tohop(k-1, n-1);
+		int C2 = tohop(k, n-1);
+		return C1 + C2;
+	}
+}
+
+void thaphn(int n, char A, char B, char C) {
+	if (n == 1) {
+		printf("Move 1 disk from %c to %c\n", A, B);
+	} else {
+		thaphn(n-1, A, C, B);
+		thaphn(1, A, B, C);
+		thaphn(n-1, C, B, A);
+	}
+}
+
+int main() {
+  printf("%d\n", tong(5));
+  printf("%d\n", tohop(2, 3));
+	thaphn(3, 'A', 'B', 'C');
+	return 0;
+}
